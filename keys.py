6ea51ba5c4771d4d8b344e52c3b2cde25bf8c890
jjpit <44243652+jjpit@users.noreply.github.com>
2018-10-17 14:38:35 -0500

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,14 @@
+# twitter bot that tweets out weather updates
+# By: JJpit
+# Date: 9-14-18
+
+# keys for the twitter bot
+
+# twitter ones
+consumer_key = "enter key here"
+consumer_secret = "enter key here"
+access_token = "enter key here"
+access_token_secret = "enter key here"
+
+# dark sky API ones
+darksky_key = "enter key here"
